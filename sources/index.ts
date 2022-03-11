78ca1ceb6f5cec20b8dc198f3f37c9847c9b9f65
alevnyacow <alevnyacow@gmail.com>
2022-03-11 19:44:18 +0300

    fixed primitives bug

--- sources/index.ts
+++ sources/index.ts
@@ -1,11 +1,13 @@
 import { v4 } from "uuid";
 import { deepProxyCreator } from "./deep-proxy-creator";
+import { isRecord } from "./utils/type-guards";
 
 export const deepProxy = (handlers: Array<(id: string) => void>, id = v4()) => {
     const handler = (id: string): ProxyHandler<object> => ({
         set: (target, prop, value) => {
-            const proxiedValue = deepProxyCreator(handler(id))(value);
-            (target as any)[prop] = proxiedValue;
+            (target as any)[prop] = isRecord(value)
+                ? deepProxyCreator(handler(id))(value)
+                : value;
             handlers.forEach((handler) => handler(id));
             return true;
         }
